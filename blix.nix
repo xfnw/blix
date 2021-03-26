cc7bffc3085094f90f2a5b792277ffc672bca0f9
xfnw <xfnw@riseup.net>
2021-03-26 19:15:12 +0000

    medusa

--- blix.nix
+++ blix.nix
@@ -84,7 +84,7 @@ TE5AAAAIDBUk5IjB3+trnVO6pncivFbOetUL8BPTl3CwAtk4532 xfnw@raven" ];
 
     # crack
     hashcat mfoc pyrit john crunch diceware crowbar
-    cowpatty bully deepsea reaverwps amass
+    cowpatty bully deepsea reaverwps amass medusa
 
     # security scan
     lynis chkrootkit
