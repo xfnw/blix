a390f2e279210bb45fe719d259150de38e24e550
xfnw <xfnw@riseup.net>
2022-04-09 20:51:15 -0400

    add whatweb

--- blix.nix
+++ blix.nix
@@ -67,7 +67,7 @@ with lib;
     bettercap stress-ng multimon-ng aircrack-ng mfcuk pixiewps nuclei
     hcxtools dirb sslsplit whsniff sniffglue pwnat cutecom minicom
     subfinder zap hping proxychains minimodem cadaver macchanger testssl
-    gnirehtet # reverse android tethering
+    gnirehtet whatweb
 
     # research
     theharvester tor
